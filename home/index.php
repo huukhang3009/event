3d78514c3165406a9968a53d681bb36cb7507fee
huukhangh <113587176+huukhangh@users.noreply.github.com>
2023-12-26 03:31:21 +0700

    first commit

---
+++ home/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="vn">
+
+<head>
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--CDN Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet" />
+    <!-- icons -->
+    <title>Trang chủ</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="bootstrap-5.1.3-dist/js/jquery-3.6.0.min.js"></script>
+
+  
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="js/script.js"></script>
+    <script src="js/showcart.js"></script>
+    <script src="js/labels.js"></script>
+    <script src="js/sw_lang.js"></script>
+</head>
+
+<body>
+     <!-- Add New Button -->
+    <a class="btn btn-block btn-sm btn-default btn-flat border-primary new_audience" href="javascript:void(0)">
+        <i class="fa fa-plus"></i> Thêm mới
+    </a>
+    <header  class="header justify-content-center">
+
+        <nav  class="navbar ">
+            <a style="text-decoration: none" href="index.php" class="multilang" id="MENU_HOME">Trang Chủ</a>
+            <a style="text-decoration: none" href="../portal.php" class="multilang" id="MENU_PRODUCT">Login</a>
+        </nav>
+
+    
+    </header>
+
+        <div class="row container-fluid">
+
+            <div class="image">
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fglints.com%2Fvn%2Fblog%2Fnhan-vien-to-chuc-su-kien-la-gi%2F&psig=AOvVaw38TXVBg7TWWdAc31FQN2xX&ust=1702210270369000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJDuucSpgoMDFQAAAAAdAAAAABAD" alt="">
+            </div>
+
+        </div>
+
+    </section>
+    
+
+
+    <section class="blogs" id="blogs">
+
+        <h1 style="color: var(--main-color);text-transform: uppercase; text-align: center;
+        font-size: 4rem;" class="multilang" id="HEAD_3"><span>Sự Kiện Sắp Diễn Ra</span> </h1>
+
+<div class="box-container">
+
+<div class="box">
+    <div class="image">
+        <img src="../uploads/ctu.jpg" alt="">
+    </div>
+    <div class="content">
+    <b><span >Báo Cáo Luận Văn KTPM</span></b>
+        <p><b>Thời gian bắt đầu: Dec 09 2023 09:36 PM</b> <br>
+		<b>Thời gian kết thúc: Dec 09 2023 09:36 PM</b> <br>
+		<b>Người phụ trách: Nguyen Hoai</b> </p>
+    </div>
+</div>
+
+<div class="box">
+    <div class="image">
+        <img src="../uploads/ctu.jpg" alt="">
+    </div>
+    <div class="content">
+    <b><span >Báo Cáo Luận Văn Tốt Nghiệp LVTN CNTT 2023</span></b>
+        <p><b>Thời Gian Bắt Đầu: Nov 27 2023 02:00 PM</b> <br>
+		<b>Thời Gian Kết Thúc: Nov 27 2023 04:15 PMM</b> <br>
+		<b> Người Phụ Trách: Nguyen Hoai</b> </p>
+    </div>
+</div>
+
+<div class="box">
+    <div class="image">
+        <img src="../uploads/ctu.jpg" alt="">
+    </div>
+    <div class="content">
+    <b><span >Hội Chợ Việc Làm CNTT</span></b>
+        <p><b>Thời Gian Bắt Đầu: Dec 09 2023 07:30 AM</b> <br>
+		<b>Thời Gian Kết Thúc: Dec 09 2023 01:00 PM</b> <br>
+		<b>Người Phụ Trách: Nguyen Hoai</b> </p>
+    </div>
+</div>
+
+        </div>
+
+    </section>
+
+</body>
+
+<footer>
+    <div class="container">
+        <!--Bắt Đầu Nội Dung Giới Thiệu-->
+        <div class="noi-dung about">
+            <h2>Về Chúng Tôi</h2>
+            <p> 
+            </p>
+            <ul class="social-icon">
+                <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                <li><a href=""><i class="fa fa-youtube"></i></a></li>
+            </ul>
+        </div>
+        <!--Kết Thúc Nội Dung Giới Thiệu-->
+        <!--Bắt Đầu Nội Dung Đường Dẫn-->
+        <div class="noi-dung links">
+            <h2>Đường Dẫn</h2>
+            <ul>
+                <li><a href="index.php">Trang Chủ</a></li>
+                <li><a href="#about">Về Chúng Tôi</a></li>
+                <li><a href="#footer">Thông Tin Liên Lạc</a></li>
+                <li><a href="#">Dịch Vụ</a></li>
+                <li><a href="#">Điều Kiện Chính Sách</a></li>
+            </ul>
+        </div>
+        <!--Kết Thúc Nội Dung Đường Dẫn-->
+        <!--Bắt Đâu Nội Dung Liên Hệ-->
+        <div class="noi-dung contact">
+            <h2>Thông Tin Liên Hệ</h2>
+            <ul class="info">
+                <li>
+                    <span><i class="fa fa-map-marker"></i></span>
+                    <span>Đường 3/2<br />
+                        Phường Xuân Khánh, Quận Ninh Kiều, Thành Phố Cần Thơ<br />
+                        Việt Nam</span>
+                </li>
+                <li>
+                    <span><i class="fa fa-phone"></i></span>
+                    <p><a href="#">+84 123 456 789</a>
+                        <br />
+                        <a href="#">+84 987 654 321</a></p>
+                </li>
+                <li>
+                    <span><i class="fa fa-envelope"></i></span>
+                    <p><a href="#"><span class="__cf_email__" data-cfemail="43272a22202b2a262e222a2f03242e222a2f6d202c2e">[Email&#160;]</span></a></p>
+                </li>
+                <li>
+                    <form class="form">
+                        <input type="email" class="form__field" placeholder="Đăng Ký Email thông báo đơn h" />
+                        <button type="button" class="btn btn--primary  uppercase">Gửi</button>
+                    </form>
+                </li>
+            </ul>
+        </div>
+        <!--Kết Thúc Nội Dung Liên Hệ-->
+    </div>
+</footer>
+
+</html>
---
+++ home/js/labels.js
@@ -0,0 +1,138 @@
+var labels={
+	//menu
+	"MENU_HOME":{
+		"vi-VN":"Trang Chủ",
+		"en-US":"Home",
+	},
+	"MENU_INTRODUCE":{
+		"vi-VN":"Giới Thiệu",
+		"en-US":"Introduce",
+	},
+	"MENU_PRODUCT":{
+		"vi-VN":"Sản Phẩm",
+		"en-US":"Product",
+	},
+	"MENU_CONTACT":{
+		"vi-VN":"Liên Hệ",
+		"en-US":"Contact",
+	},
+	"MENU_LOGIN":{
+		"vi-VN":"Đăng Nhập",
+		"en-US":"Login",
+	},
+    "MENU_BLOGS":{
+		"vi-VN":"Tin Công Nghệ",
+		"en-US":"Teach News",
+	},
+	// Trang Chu
+    "HEAD_1":{
+		"vi-VN":"Uy tín tạo nên thương hiệu, Niềm tin đi cùng chất lượng",
+		"en-US":"Prestige creates brand, trust goes with quality",
+	},
+    "HEAD_2":{
+		"vi-VN":"Chào Quý Khách",
+		"en-US":"Hello Guests",
+	},
+    "HEAD_3":{
+		"vi-VN":"Tin Công Nghệ",
+		"en-US":"Teach News",
+	},
+    "HEAD_4":{
+		"vi-VN":"Chúng Tôi Đặc Biệt ở Điều Gì?",
+		"en-US":"What Are We Special About?",
+	},
+    "HEAD_5":{
+		"vi-VN":"Mobile Fun Shop là cửa hàng chuyển cung cấp các dòng điện thoại chính hãng như: Iphone, Samsung, Oppo, Sony, Vivo,... cao cấp cho người dùng, liệu bạn có biết?",
+		"en-US":"Mobile Fun Shop is a shipping store that offers genuine phones such as iPhone, Samsung, Oppo, Sony, Vivo,... High-end for users, do you know?",
+	},
+    "HEAD_6":{
+		"vi-VN":"Nếu bạn muốn đến với chúng tôi thì hãy đến địa chỉ : Đường 3/2, Phường Xuân Khánh, Quận Ninh Kiều, TP.Cần Thơ",
+		"en-US":"If you want to come to us, go to the address: 3/2 Street, Xuan Khanh Ward, Ninh Kieu District, Can Tho City",
+	},
+    "HEAD_7":{
+		"vi-VN":"Xem thêm",
+		"en-US":"Read More",
+	},
+    "HEAD_8":{
+		"vi-VN":"Hai mẫu Galaxy khiến người dùng phân vân khi nâng cấp vì đều rất tuyệt.",
+		"en-US":"The two Galaxy models make users wonder when upgrading because they are both great.",
+	},
+    "HEAD_9":{
+		"vi-VN":"Những Điễm hiếm hoi Hé Lộ về sự ra mắt của Iphone 14 trong tháng 9 này!",
+		"en-US":"The rare revelations about the launch of the iPhone 14 this September!",
+	},
+    "HEAD_10":{
+		"vi-VN":"Với thông tin iPhone 14 Series sẽ được Apple ra mắt vào tháng 9/2022, dự kiến sẽ có những đột phá mới về thiết kế, điều đó đã để lại rất nhiều sự mong đợi cho giới yêu thích công nghệ và để đáp trả sự mong mỏi của iFan Dựa vào đó kênh YouTube ios beta news đã đăng tải một video.....",
+		"en-US":"With the information iPhone 14 Series to be launched by Apple in September 2022, it is expected that there will be new breakthroughs in design, that will return a lot of expectations for technology lovers and to reply. iFan's wish. Based on that. YouTube channel #ios beta news posted a video .....",
+	},
+    "HEAD_11":{
+		"vi-VN":"Với thông tin iPhone 14 Series sẽ được Apple ra mắt vào tháng 9/2022, dự kiến sẽ có những đột phá mới về thiết kế, điều đó đã để lại rất nhiều sự mong đợi cho giới yêu thích công nghệ và để đáp trả sự mong mỏi của iFan Dựa vào đó kênh YouTube ios beta news đã đăng tải một video.....",
+		"en-US":"Having a more powerful processor, a new design and running on a newer version of Android, are those improvements worth the upgrade for Galaxy S20 FE users to the S21 FE? Galaxy S21 FE uses a metal frame, glass front and plastic back, similar to the Galaxy S20 FE. However, the new model....",
+	},
+    "HEAD_12":{
+		"vi-VN":"Sony mới đây đã trình làng thế hệ flagship Xperia 1 IV mới nhất với nhiều nâng cấp về mặt phần cứng, đặc biệt là về camera. Phiên bản Mark IV năm nay chia sẻ nhiều điểm chung giữa Xperia Pro và Pro-I. Xperia 1 IV ra mắt.....",
+		"en-US":"Sony has recently launched the latest generation of flagship Xperia 1 IV with many upgrades in terms of hardware, especially in camera. This year's version of Mark IV shares many similarities between the Xperia Pro and Pro-I. Xperia 1 IV launched.....",
+	},
+    "HEAD_13":{
+		"vi-VN":"Về Chúng Tôi",
+		"en-US":"About Us",
+	},
+    "HEAD_15":{
+		"vi-VN":"Đường Dẫn",
+		"en-US":"Link",
+	},
+    "HEAD_16":{
+		"vi-VN":"Về Chúng Tôi",
+		"en-US":"About Us",
+	},
+    "HEAD_17":{
+		"vi-VN":"Địa Chỉ",
+		"en-US":"Address",
+	},
+    "HEAD_18":{
+		"vi-VN":"Dịch Vụ",
+		"en-US":"Service",
+	},
+    "HEAD_19":{
+		"vi-VN":"Điều Kiện Chính Sách",
+		"en-US":"Policy Conditions",
+	},
+    "HEAD_20":{
+		"vi-VN":"Đường 3/2",
+		"en-US":"Street 3/2",
+	},
+    "HEAD_21":{
+		"vi-VN":"Sony mới đây đã trình làng thế hệ flagship Xperia 1 IV mới nhất với nhiều nâng cấp về mặt phần cứng, đặc biệt là về camera. Phiên bản Mark IV năm nay chia sẻ nhiều điểm chung giữa Xperia Pro và Pro-I. Xperia 1 IV ra mắt.....",
+		"en-US":"Sony has recently launched the latest generation of flagship Xperia 1 IV with many upgrades in terms of hardware, especially in camera. This year's version of Mark IV shares many similarities between the Xperia Pro and Pro-I. Xperia 1 IV launched.....",
+	},
+    "HEAD_20":{
+		"vi-VN":"Đường 3/2",
+		"en-US":"Street 3/2",
+	},
+    "HEAD_21":{
+		"vi-VN":"Sản Phẩm Nổi Bật",
+		"en-US":"OUTSTANDING PRODUCTS",
+	},
+    "HEAD_22":{
+		"vi-VN":"Đặt hàng",
+		"en-US":"Place an order",
+	},
+    "HEAD_23":{
+		"vi-VN":"LIÊN HỆ VỚI CHÚNG TÔI",
+		"en-US":"CONTACT US",
+	},
+    "HEAD_24":{
+		"vi-VN":"Tên",
+		"en-US":"Name",
+	},
+    "HEAD_25":{
+		"vi-VN":"Số Điện Thoại",
+		"en-US":"Phone",
+	},
+    "HEAD_26":{
+		"vi-VN":"Gửi ngay bây giờ",
+		"en-US":"Send Now",
+	},
+
+};
+
